d6f437182e2dfb8eb78b71ea7a6b3072d73f38ad
robertoschuster <robertoaschuster@gmail.com>
2020-02-13 22:45:23 -0300

    Melhoria na autenticação do usuário no middleware de autenticação.

--- fast-feet/src/app/middlewares/auth.js
+++ fast-feet/src/app/middlewares/auth.js
@@ -2,12 +2,12 @@ import jwt from 'jsonwebtoken';
 import { promisify } from 'util';
 import authConfig from '../../config/auth';
 
+import User from '../models/User';
+
 // Middleware de autenticação
 // verifica se o usuário está autenticado através de um token jwt na requisição
 export default async (req, res, next) => {
-  // Tenta obter token JWT da requisição
   const authHeader = req.headers.authorization;
-  // console.log(authHeader);
 
   // Se o token não foi informado, retorna erro
   if (!authHeader) {
@@ -20,18 +20,26 @@ export default async (req, res, next) => {
    * descarta a primeira parte
    */
   const [, token] = authHeader.split(' ');
-  // console.log(token);
 
   try {
     // Decodifica o token
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
-    // console.log(decoded);
+
+    if (!decoded.id) {
+      return res.status(401).json({ error: 'No user provided in the token' });
+    }
+    const user = await User.findByPk(decoded.id);
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ error: 'The user provided in the token was not found' });
+    }
 
     // Inclui o id do usuário na própria requisição para ser acessado nas próximas funções
     req.userId = decoded.id;
     return next();
   } catch (error) {
-    // console.log(error);
     return res.status(401).json({ error: 'Token invalid' });
   }
 };
