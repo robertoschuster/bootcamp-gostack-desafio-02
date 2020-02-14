2ec57426af9b9f632463620d8319767fc3ae631f
robertoschuster <robertoaschuster@gmail.com>
2020-02-13 21:38:27 -0300

    Model de usuários.

--- fast-feet/src/database/index.js
+++ fast-feet/src/database/index.js
@@ -1,9 +1,21 @@
 import Sequelize from 'sequelize';
-import dbConfig from '../config/database';
+import databaseConfig from '../config/database';
+import User from '../app/models/User';
+
+// Array para registrar todos os models da aplicação
+const models = [User];
 
 class Database {
   constructor() {
-    this.connection = new Sequelize(dbConfig);
+    this.init();
+  }
+
+  init() {
+    // Conecta com o banco
+    this.connection = new Sequelize(databaseConfig);
+
+    // Carrega (registra) todos os models
+    models.map(model => model.init(this.connection));
   }
 }
 
